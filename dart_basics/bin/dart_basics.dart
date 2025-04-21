a58b8ffae3e5c8c2b7a0912e3954ff4bd546a3a1
Mohammed Abuyahya <mabuyahy@c2r3s9.42amman.com>
2025-04-21 16:06:30 +0300

    just started

---
+++ dart_basics/bin/dart_basics.dart
@@ -0,0 +1,4 @@
+import 'package:dart_basics/dart_basics_test.dart' as dart_basics;
+
+void main(List<String> arguments) {
+}
---
+++ dart_basics/test/dart_basics_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_basics/dart_basics_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
